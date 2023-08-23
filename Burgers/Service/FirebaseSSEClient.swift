8c5b177f471937dc8c754f02405c0fb8d60bfb21
Vadim Kuzmin <kuzminvadima@yandex.ru>
2023-08-23 10:48:41 +0300

    Added SSE client

---
+++ Burgers/Service/FirebaseSSEClient.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseSSEClient.swift
+//  Burgers
+//
+//  Created by Вадим Кузьмин on 19.08.2023.
+//
+
+import Foundation
---
+++ Burgers/TabBarController/TabBarController.swift
@@ -0,0 +1,8 @@
+//
+//  TabBarController.swift
+//  Burgers
+//
+//  Created by Вадим Кузьмин on 11.08.2023.
+//
+
+import Foundation
